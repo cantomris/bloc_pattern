3e77bddf8affdda65c3d29c3981ad101e3830b98
cantomris <c.tomris@gezinomi.com>
2022-06-14 11:39:26 +0300

    Initial commit

---
+++ lib/counter_bloc.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+enum CounterAction {
+  Increment, Decrement, Reset
+}
+class CounterBloc {
+  int counter = 0;
+  final _stateStreamController = StreamController<int>();
+  StreamSink<int> get counterSink => _stateStreamController.sink;
+  Stream<int> get counterStream => _stateStreamController.stream;
+
+  final _eventStreamController = StreamController<CounterAction>();
+  StreamSink<CounterAction> get eventSink => _eventStreamController.sink;
+  Stream<CounterAction> get eventStream => _eventStreamController.stream;
+
+  CounterBloc() {
+    counter = 0;
+    eventStream.listen((event) {
+      if (event == CounterAction.Increment) counter++;
+      else if (event == CounterAction.Decrement && counter > 0) counter--;
+      else if (event == CounterAction.Reset) counter = 0;
+
+      counterSink.add(counter);
+    });
+  }
+
+  void dispose() {
+    _stateStreamController.close();
+    _eventStreamController.close();
+  }
+}
